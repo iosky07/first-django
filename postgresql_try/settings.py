b6c73449f150c4277b982181fe34e39ed3259214
iosky07 <yoskitanjung07@gmail.com>
2021-01-18 23:42:09 +0700

    add migrations

--- postgresql_try/settings.py
+++ postgresql_try/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'postgresql_try.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'simple_web',
         'USER': 'postgres',
         'PASSWORD': 'yoskiapik07',
         'HOST': 'localhost',
--- simple_web/models.py
+++ simple_web/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    thumbnail = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+class BlogDetail(models.Model):
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True)
+    value = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
