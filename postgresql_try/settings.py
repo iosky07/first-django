c55cd94df12118ba9e120e34367c851acd9e073f
iosky07 <yoskitanjung07@gmail.com>
2021-01-19 12:40:13 +0700

    first form create

--- postgresql_try/settings.py
+++ postgresql_try/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'simple_web',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- simple_web/views.py
+++ simple_web/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .forms import BlogForm
+
 # Create your views here.
 def home(request):
     blogs = Blog.objects.all()
@@ -10,15 +12,21 @@ def blogs(request):
     blogs = Blog.objects.all()
     return render(request, 'pages/blogs/blogs.html', {'blogs' : blogs})
 
-def createBlog(request):
-    return render(request, 'pages/blogs/create.html')
+def createTag(request):
+    return render(request, 'pages/tags/create.html')
 
 def tags(request):
     tags = Tag.objects.all()
     return render(request, 'pages/tags/tags.html', {'tags' : tags})
 
-def createTag(request):
-    return render(request, 'pages/tags/create.html')
+def createBlog(request, template_name='forms/blog_form.html', form_class=BlogForm):
+    if request.method == 'POST':
+        form = form_class(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = form_class()
+    return render(request, template_name, {'form': form})
 
 def comments(request):
     comments = Comment.objects.all()
